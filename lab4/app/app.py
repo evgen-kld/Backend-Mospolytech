7b48006df7ab0d3bf7c0d6d4414d1681b0d0cfb0
evgen-kld <evgenkld@inbox.ru>
2022-04-20 18:40:50 +0300

    lab4 update

--- lab4/app/app.py
+++ lab4/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from mysql_db import MySQL
+import mysql.connector as connector
 
 
 
@@ -15,6 +16,20 @@ app.config.from_pyfile('config.py')
 login_manager.init_app(app)
 
 mysql = MySQL(app)
+
+CREATE_PARAMS = ['login', 'password', 'first_name', 'last_name', 'middle_name', 'role_id']
+
+def request_params(params_list):
+    params = {}
+    for param_name in params_list:
+        params[param_name] = request.form.get(param_name) or None
+    return params
+
+def load_roles():
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute('SELECT id, name FROM roles;')
+        roles = cursor.fetchall()
+    return roles
 
 class User(UserMixin):
     def __init__(self, user_id, login):
@@ -41,13 +56,13 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login = request.form.get('login')
+        login_ = request.form.get('login')
         password = request.form.get('password')
         remember_me = request.form.get('remember_me') == 'on'
         with mysql.connection.cursor(named_tuple=True) as cursor:
             cursor.execute(
                 'SELECT * FROM users WHERE login=%s AND password_hash=SHA2(%s, 256);',
-                (login, password)
+                (login_, password)
             )
             db_user = cursor.fetchone()
         if db_user:
@@ -61,3 +76,32 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/users')
+def users():
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute('SELECT users.*, roles.name as role_name FROM users LEFT JOIN roles ON users.role_id=roles.id;')
+        users = cursor.fetchall()
+    return render_template('users/index.html', users=users)
+
+@app.route('/users/new')
+@login_required
+def new():
+    return render_template('users/new.html', user={}, roles=load_roles())
+
+@app.route('/users/create', methods=['POST'])
+@login_required
+def create():
+    params = request_params(CREATE_PARAMS)
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        try:
+            cursor.execute(('INSERT INTO users (login, password_hash, last_name, first_name, middle_name, role_id)'
+            'VALUES (%(login)s, SHA2(%(password)s, 256), %(last_name)s, %(first_name)s, %(middle_name)s, %(role_id)s);'),
+            params
+            )
+            mysql.connection.commit()
+        except connector.Error:
+            flash('Введены некоректные данные. Ошибка сохранения', 'danger')
+            return render_template('users/new.html', user=params, roles=load_roles())
+    flash(f'Пользователь {params.get("login")} был успешно создан!', 'success')
+    return redirect(url_for('users'))
