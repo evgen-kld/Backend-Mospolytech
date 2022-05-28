4b344bc7d909a618d1ceeccbc60090944bb2a50a
evgen-kld <evgenkld@inbox.ru>
2022-05-28 15:20:07 +0300

    lab5

---
+++ lab5/app/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '123456qwerty'
+MYSQL_USER = 'std_1760_lab4'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_1760_lab4'
+MYSQL_PASSWORD = '12345678'
+ADMIN_ROLE_ID = 1
