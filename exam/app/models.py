fad516b5cf416d625351beabd6b0f46a938a0da8
evgen-kld <evgenkld@inbox.ru>
2022-06-20 23:44:16 +0300

    Добавление всех возможных моделей в models.py

--- exam/app/models.py
+++ exam/app/models.py
@@ -2,6 +2,8 @@ from app import db
 import sqlalchemy as sa
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import os
+from flask import url_for
 
 
 class User(db.Model, UserMixin):
@@ -23,7 +25,6 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -32,3 +33,66 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<User: %r>' % self.role_name
+
+
+
+
+books_genres = db.Table('books_genres',
+    db.Column('books_id', db.Integer, db.ForeignKey('books.id'), primary_key=True),
+    db.Column('genres_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
+)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False, unique=True)
+    short_description = db.Column(db.Text)
+    year = db.Column(db.Date, nullable=False)
+    publisher = db.Column(db.String(30), nullable=False)
+    author = db.Column(db.String(30), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<User: %r>' % self.title
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+    id = db.Column(db.Integer, primary_key=True)
+    genre_name = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return '<User: %r>' % self.genre_name
+
+class Cover(db.Model):
+    __tablename__ = 'covers'
+    id = db.Column(db.String(100), primary_key=True)
+    file_name = db.Column(db.String(100), nullable=False)
+    mime_type = db.Column(db.String(100), nullable=False)
+    md5_hash = db.Column(db.String(100), nullable=False, unique=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), unique=True)
+
+    def __repr__(self):
+        return '<Image: %r>' % self.file_name
+
+    @property
+    def storage_filename(self):
+        _, ext = os.path.splitext(self.file_name)
+        return self.id + ext
+
+    @property
+    def url(self):
+        return url_for('image', image_id=self.id)
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=sa.sql.func.now())
+
+    def __repr__(self):
+        return f'<Reviews: {self.user_id} {self.created_at}>'
+
